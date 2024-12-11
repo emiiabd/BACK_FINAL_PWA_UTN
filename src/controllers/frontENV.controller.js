214b1ca244045055c6533222565e244e40954110
emiiabd <abdalaemiliano@gmail.com>
2024-12-11 17:57:25 -0300

    keys

--- src/controllers/frontENV.controller.js
+++ src/controllers/frontENV.controller.js
@@ -18,4 +18,17 @@ const getGoogleMapsFrontApiKey = async (req, res) => {
   }
 }
 
-export { getGoogleMapsFrontApiKey }
+const getReCaptchaFrontApiKey = async (req, res) => {
+  try {
+    const G_CAPTCHA_API_KEY = process.env.G_CAPTCHA_FRONT_KEY
+
+    const response = response200('OK', { api_key: G_CAPTCHA_API_KEY })
+    return res.status(200).json(response)
+  }
+  catch (error) {
+    const response = response500('INTERNAL SERVER ERROR', { detail: error.message })
+    return res.status(500).json(response)
+  }
+}
+
+export { getGoogleMapsFrontApiKey, getReCaptchaFrontApiKey }
--- src/routes/frontENV.router.js
+++ src/routes/frontENV.router.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getGoogleMapsFrontApiKey } from '../controllers/frontENV.controller.js';
+import { getGoogleMapsFrontApiKey, getReCaptchaFrontApiKey } from '../controllers/frontENV.controller.js';
 
 const frontENVrouter = express.Router();
 
 frontENVrouter.get('/GM-ak', getGoogleMapsFrontApiKey);
+frontENVrouter.get('/GC-ak', getReCaptchaFrontApiKey);
 
 export default frontENVrouter;
