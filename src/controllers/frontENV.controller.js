fc593898e80bf5e6e2861b75509242ce852c091f
emiiabd <abdalaemiliano@gmail.com>
2024-12-11 17:33:56 -0300

    env

--- src/controllers/frontENV.controller.js
+++ src/controllers/frontENV.controller.js
@@ -7,7 +7,9 @@ import { response200, response500 } from "../utils/responses.util.js"
 
 const getGoogleMapsFrontApiKey = async (req, res) => {
   try {
-    const response = response200('OK', { api_key: process.env.G_MAPS_FRONT_KEY })
+    const G_MAPS_API_KEY = process.env.G_MAPS_FRONT_KEY
+
+    const response = response200('OK', { api_key: G_MAPS_API_KEY })
     return res.status(200).json(response)
   }
   catch (error) {
